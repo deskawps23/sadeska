cea9359b0baf97309269a2a3ee230a564d16134f
deskawps23 <deskawiyatapraja@gmail.com>
2026-08-03 08:49:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
+  apiKey: "AIzaSyA...", // Masukkan API Key lengkap asli Anda di sini
   authDomain: "si-deska-wps.firebaseapp.com",
   databaseURL: "https://si-deska-wps-default-rtdb...",
   projectId: "si-deska-wps",
   storageBucket: "si-deska-wps.appspot.com",
   messagingSenderId: "595163960871",
-  appId: "1:595163960871:web:..."
+  appId: "1:595163960871:web:..." // Masukkan App ID lengkap asli Anda di sini
 };
